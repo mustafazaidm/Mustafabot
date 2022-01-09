7a79e33d0cd49698d747793c3d728885d81d8c8e
mustafazaidm <97402642+mustafazaidm@users.noreply.github.com>
2022-01-09 21:54:03 +0300

    Add files via upload

---
+++ bot/includes/configuration.inc.php
@@ -0,0 +1,14 @@
+<?
+// Yassinox Botnet Configuration File \\
+$installed = 0; // If You See This Command, Please Change 0 to 1
+// Let's move Now To Mysql Configuration !
+$usernamedb = "root"; // Just Put Your Mysql Username
+$passworddb = "12345678"; // Just Put Your Mysql Password
+$hostdb = "localhost"; // I think It's For Mysql Hostname
+$db = "bot"; // Just Fill The Database Name Here
+// Do I Need To Login ? Yes i need
+$usernamec = "mustafa"; // Put The Prefered Username
+$passwordc = "mustafa"; // Just put A Strong Password
+$security_token = "MAKE A RANDOM STRING FOR YOUR TOKEN"; // This Is Will Be Your Session Token !
+// And, That's All !
+?>
---
+++ bot/includes/mysql_connect.inc.php
@@ -0,0 +1,5 @@
+<?
+include("configuration.inc.php");
+$connect = mysql_connect($hostdb,$usernamedb,$passworddb);
+$selectdb = mysql_select_db($db);
+?>
